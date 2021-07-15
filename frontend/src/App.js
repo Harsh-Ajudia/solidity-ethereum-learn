fb719aaa02819db1d63e4908750a31300cd79f71
Neosoft <harshajudia99@gmail.com>
2021-07-15 11:25:28 +0530

    added react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3'
+import Lottery from './Lottery'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log('useeffect')
+  }, [])
+  const title = 'Lottery'
+  console.log(web3.version)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
     </div>
   );
 }
--- solidity/deploy_lottery.js
+++ solidity/deploy_lottery.js
@@ -1,12 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 const { interface, bytecode } = require('./compile_lottery')
-
+const dotenv = require('dotenv');
+dotenv.config();
 const provider = new HDWalletProvider(
     process.env.METAMASK_MNEMONIC,
     process.env.RINKEBY_URL_INFURA
 )
-
 const web3 = new Web3(provider)
 
 const deploy = async () => {
@@ -18,6 +18,7 @@ const deploy = async () => {
 
     console.log(`deployed from ${account}`)
     console.log(`deployed to ${result.options.address}`)
+    console.log(interface)
 }
 
 deploy()
